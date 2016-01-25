477e9e4396e3ab42b2938cb4bc26c6d1bfaca829
Wendy Hernández <wahaf13@hotmail.com>
2016-01-25 00:18:13 -0600

    Initial Commit

---
+++ isbn_tarea2Tests/isbn_tarea2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  isbn_tarea2Tests.swift
+//  isbn_tarea2Tests
+//
+//  Created by Wendy Hernandez on 25/01/16.
+//  Copyright © 2016 Wendy Hernandez. All rights reserved.
+//
+
+import XCTest
+@testable import isbn_tarea2
+
+class isbn_tarea2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
