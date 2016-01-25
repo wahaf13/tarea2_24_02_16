ff471fcc8fd6bc973d7b39b9db4317b60088a8ed
Wendy Hernández <wahaf13@hotmail.com>
2016-01-25 00:27:39 -0600

    ya

--- isbn_tarea2/ViewController.swift
+++ isbn_tarea2/ViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var busca: UISearchBar!
+    @IBOutlet weak var resultado: UITextView!
+    @IBOutlet weak var etiqueta1: UILabel!
+    @IBOutlet weak var etiqueta2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func petocion(sender: UISearchBar){
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"+busca.text!
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        /*let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        resultado.text = texto! as String*/
+        
+        do{
+            
+            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+            let dico1 = json as! NSDictionary
+            let dico2 = dico1["publishers"] as! NSDictionary
+            let dico3 = dico2["goodreads"] as! NSDictionary
+            self.etiqueta1.text = dico3["title"] as! NSString as String
+            
+        }
+        catch _{
+            
+        }
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
